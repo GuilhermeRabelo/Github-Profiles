2466c3bc214516f25bc1648f2c67193f0b1ba8b2
Guilherme Rabelo <gui_rabelo@hotmail.com>
2020-09-16 19:00:18 -0300

    wip(home) add ReactiveFormsModule to catch input value

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  form: FormGroup;
+  loading: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
+    this.generateForm();
+  }
+
+  generateForm(){
+    this.form = new FormGroup({
+      username : new FormControl('', [Validators.required]),
+    });
+  }
+
+  search(){
+    console.log(this.form.value);
   }
 
 }
